80e0ed66b63d76540f1046d6ce1d12118289da5d
akshaybhalotia <abhalotia99@gmail.com>
2016-01-18 08:34:04 +0530

    UI enhancements

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,6 +2,8 @@ class Search < ActiveRecord::Base
   validates_presence_of :from_location, :to_location
   validates :date_of_journey, date: true
   validate :date_of_journey_is_not_less_than_today
+
+  LOCATIONS = ["", "A", "B", "C"]
   
   def date_of_journey_is_not_less_than_today
     errors.add(:date_of_journey, "must be today or after today") if (date_of_journey.nil? || date_of_journey < Date.today)
